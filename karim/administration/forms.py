c0fdf65ce786f8033da55931f33c2679d9ffaaae
Hassane <imthassane@gmail.com>
2018-07-21 15:52:54 +0200

    Formulaire d'ajout des produits ajouté et fonctionnel

---
+++ karim/administration/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from main.models import Category
+
+CATEGORIES = ((cat.name, cat.name) for cat in Category.objects.filter(visible=True))
+
+class ProductForm(forms.Form):
+    
+    auto_id = True
+    
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'uk-input',
+        'placeholder': "Ex: table"
+    }))
+
+    category = forms.CharField(widget=forms.SelectMultiple(attrs={
+        'class': 'uk-select'
+    }, choices=CATEGORIES))
+
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'id': 'description',
+        'class': 'uk-input',
+        'placeholder': "Entrez la description du produit",
+        "row": "10"
+    }))
+
+    price = forms.DecimalField(max_digits=7, decimal_places=2, widget=forms.NumberInput(attrs={
+        'class': 'uk-input',
+        'placeholder': "Entrez le prix du produit",
+    }))
+
+    city = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'uk-input',
+        'placeholder': "Ce produit est dans quel ville ?"
+    }))
+
+    quantity = forms.CharField(widget=forms.NumberInput(attrs={
+        'class': 'uk-input',
+        'placeholder': "Quelle est la quantité du produit en stock ?"
+    }))
+
+    availability = forms.CharField(widget=forms.DateTimeInput(attrs={
+        'class': 'uk-input',
+        'placeholder': "Format: jj/mm/aa hh:mm"
+    }))
+
+    pictures = forms.FileField(widget=forms.ClearableFileInput(attrs={
+        'multiple': True
+    }))
--- karim/karim/functions.py
+++ karim/karim/functions.py
@@ -6,3 +6,28 @@ def handle_upload_file(folder, file_name, f):
     with open(file, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+
+# Fonction qui va parser le resultat du forms.CharField
+# avec le widget SelectMultipleField en liste au lieu d'une chaine
+# de caractère
+def parse_select_multiple(string):
+
+    datas = []
+
+    begin = False
+    value = ""
+
+    for s in string:
+        
+        if s == '\'':
+            begin = not begin
+        
+        if begin:
+            if s not in "'[], ":
+                value += s
+        else:
+            datas.append(value)
+            value = ""
+
+    datas = list(filter(lambda x: len(x) > 1, datas))
+    return datas
